6592d13ecff25ad9a64b1d6c97dbacd1e5b9bf2a
William Barela <william.barela.dev@gmail.com>
2018-11-05 09:19:04 -0600

    abstract original code to functions

--- query/__init_.py
+++ query/__init_.py
@@ -3,20 +3,30 @@ from Bio import Entrez
 from urllib.request import urlopen
 import csv
 
-Entrez.email = "william.barela@ttu.edu"
-handle = Entrez.esearch(db="sra", term="heterodera[Organism]",retmax=1000)
-ncbi_dict = Entrez.read(handle)
-count = ncbi_dict["Count"]
-id_list = ncbi_dict['IdList']
+def get_ncbi_ids(email, database, search_term, maximum_returned_items):
+    Entrez.email = email 
+    search_results_xml = Entrez.esearch(db=database, term=search_term, retmax=maximum_returned_items)
+    ncbi_dict = Entrez.read(search_results_xml)
+    count = ncbi_dict["Count"]
+    id_list = ncbi_dict['IdList']
 
-search_post_request = Entrez.read(Entrez.epost("sra",id=",".join(id_list)))
-webenv = search_post_request["WebEnv"]
-query_key = search_post_request["QueryKey"]
+    return (count, id_list) 
 
-url = 'https://www.ncbi.nlm.nih.gov/portal/utils/file_backend.cgi?Db=sra&HistoryId=' + webenv + '&QueryKey=' + query_key + '&Sort=&Filter=all&CompleteResultCount=' + count + '&Mode=file&View=docsumcsv&p$l=Email&portalSnapshot=%2Fprojects%2FSequences%2FSeqDbRelease%401.90&BaseUrl=&PortName=live&FileName=&ContentType=csv'
-response = urlopen(url)
+def get_search_cache_keys(email, database, search_term, maximum_returned_items):
+    count, id_list = get_ncbi_ids(email, database, search_term, maximum_returned_items)
 
-csv_string = response.read().decode()
-csv_data = csv.reader(csv_string.splitlines(), delimiter=",", quotechar='"')    # this is a generator, so you need to do the next line
-csv_list = list(csv)
+    search_post_request = Entrez.read(Entrez.epost("sra",id=",".join(id_list)))
+    webenv = search_post_request["WebEnv"]
+    query_key = search_post_request["QueryKey"]
 
+    return (count, id_list, webenv, query_key)
+
+def get_csv(email, database, search_term, maximum_returned_items, download=False):
+    url = 'https://www.ncbi.nlm.nih.gov/portal/utils/file_backend.cgi?Db=sra&HistoryId=' + webenv + '&QueryKey=' + query_key + '&Sort=&Filter=all&CompleteResultCount=' + count + '&Mode=file&View=docsumcsv&p$l=Email&portalSnapshot=%2Fprojects%2FSequences%2FSeqDbRelease%401.90&BaseUrl=&PortName=live&FileName=&ContentType=csv'
+    response = urlopen(url)
+
+    csv_string = response.read().decode()
+    csv_data = csv.reader(csv_string.splitlines(), delimiter=",", quotechar='"')    # this is a generator, so you need to do the next line
+    csv_list = list(csv)
+
+    return csv_list
