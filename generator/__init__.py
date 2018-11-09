3704da9dcb0331aaf5e6c43862ac1c2d5aea5fe5
William Barela <william.barela.dev@gmail.com>
2018-11-08 23:51:06 -0600

    add skeleton of classes for generating objects to structure data

---
+++ generator/__init__.py
@@ -0,0 +1,24 @@
+class Summary(object):
+    '''
+    Takes in a row from the summary_csv list of lists and creates and object
+    '''
+
+    def init(self, summary_row):
+        '''add code'''
+
+
+class Runinfo(object):
+    '''
+    Takes in a row from the runinfo_csv list of lists and creates and object
+    '''
+
+    def init(self, runinfo_row):
+        '''add code'''
+
+class Projects(object):
+    '''
+    Takes in summary and runinfo object and joins them into a project object
+    '''
+
+    def init(self, summary_object, runinfo_object):
+        '''add code'''
