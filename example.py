a7a9633df63897164e493d6b617e1df3c0f73d9b
William Barela <william.barela.dev@gmail.com>
2018-11-10 16:46:32 -0600

    modify example.py to show how to use summary_objects and runinfo_objects

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
 from ncbi_csv_tool import query as q
+from ncbi_csv_tool import generator as gen
 
 """
     Be sure to move this file one folder up so that ncbi_csv_tool directory is on the same level.
 """
 
-summary_csv_list, run_csv_list = q.get_csv_lists("your.email@domain.com","sra","heterodera[Organism]")
+summary_csv_list, run_csv_list = q.get_csv_lists("william.barela@ttu.edu","sra","heterodera[Organism]")
+
+summary_objects_list = gen.summary_objects(summary_csv_list)
+runinfo_objects_list = gen.runinfo_objects(run_csv_list)
